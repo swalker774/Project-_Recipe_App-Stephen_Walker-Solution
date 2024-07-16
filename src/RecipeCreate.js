5869e2cb8409ccef066ad44d9126ceb8080a6f12
Stephen Walker <35walke774@gmail.com>
2024-07-16 12:20:25 -0400

    added more styling

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,56 +1,51 @@
 import React, { useState } from "react";
 
 function RecipeCreate({ addRecipe }) {
-  
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
-  
   const initialFormState = {
     name: "",
     cuisine: "",
     photo: "",
     ingredients: "",
     preparation: "",
-  }
-  
-  const [formData, setFormData] = useState({ ...initialFormState })
- 
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormState });
+
   const handleChange = (event) => {
-    const { name, value } = event.target
-    setFormData({  ...formData, [name]: value })
-  }
-  
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const handleSubmit = (event) => {
-   event.preventDefault()
-    addRecipe(formData)
-    setFormData({  ...initialFormState })
-  }
-  
+    event.preventDefault();
+    addRecipe(formData);
+    setFormData({ ...initialFormState });
+  };
+
   return (
-    <form data-testid= "recipe-form" onSubmit={handleSubmit} name="create">
+    <form data-testid="recipe-form" onSubmit={handleSubmit} name="create">
       <table>
         <tbody>
           <tr>
             <td>
-            <input 
-              name="name"
-              type="text"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Name"
-              required
+              <input
+                name="name"
+                type="text"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Name"
+                required
               />
             </td>
             <td>
-              <input 
+              <input
                 name="cuisine"
                 type="text"
                 value={formData.cuisine}
                 onChange={handleChange}
                 placeholder="Cuisine"
                 required
-                />
+              />
             </td>
             <td>
               <input
@@ -60,27 +55,25 @@ function RecipeCreate({ addRecipe }) {
                 onChange={handleChange}
                 placeholder="URL"
                 required
-                />
+              />
             </td>
             <td className="content_td">
               <textarea
                 name="ingredients"
-                type="text"
                 value={formData.ingredients}
                 onChange={handleChange}
                 placeholder="Ingredients"
                 required
-                />
+              />
             </td>
             <td className="content_td">
               <textarea
                 name="preparation"
-                type="text"
                 value={formData.preparation}
                 onChange={handleChange}
-                placeholder="preparation"
+                placeholder="Preparation"
                 required
-                />
+              />
             </td>
             <td>
               <button type="submit">Create</button>
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 function RecipeList({ recipes, deleteRecipe }) {
-  
-  // TODO: Display the list of recipes using the structure of table that is provided.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
-
   return (
     <div className="recipe-list">
       <table>
@@ -25,15 +20,14 @@ function RecipeList({ recipes, deleteRecipe }) {
               <td>{recipe.name}</td>
               <td>{recipe.cuisine}</td>
               <td>
-                <img src={recipe.photo} alt={recipe.name} style={{width: "75px", height: "75px"}} />
-                 </td>
+                <img src={recipe.photo} alt={recipe.name} />
+              </td>
               <td>{recipe.ingredients}</td>
               <td>{recipe.preparation}</td>
               <td>
-                <button
-                  name="delete"
-                  onClick={() => deleteRecipe(recipe.name)}
-                >Delete</button>
+                <button name="delete" onClick={() => deleteRecipe(recipe.name)}>
+                  <i className="fas fa-trash"></i> Delete
+                </button>
               </td>
             </tr>
           ))}
